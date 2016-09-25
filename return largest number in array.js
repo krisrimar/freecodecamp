db181c5d8259b6f11e24720b44484b17f0ee4954
krisrimar <rimarkris@gmail.com>
2016-09-25 18:30:11 +0300

    Better solution (from Internet)
    Turns out, as always, you can use “map” to do shit with arrays. Simple
    as that. But I did not know about the existence of such a function as
    “reduce”

--- return largest number in array.js
+++ return largest number in array.js
@@ -1,14 +1,10 @@
 function largestOfFour(arr) {
-    // You can do this!
-    var i, k, newArr = [];
-    for (i = 0; i < arr.length; i++) {
-        newArr[i] = arr[i][0];
-        for (k = 1; k < arr[i].length; k++) {
-            arr[i][k] > newArr[i] ? newArr[i] = arr[i][k] : newArr[i];
-        }
-    }
-    console.log(newArr);
-    return newArr;
+  return arr.map(function(group){
+    return group.reduce(function(prev, current) {
+      return (current > prev) ? current : prev;
+    }, 0);
+  });
+
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
