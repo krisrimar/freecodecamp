a4ec3da9415c9e67cce64c24da356b64021d79ab
krisrimar <rimarkris@gmail.com>
2016-09-25 19:03:28 +0300

    Better solution (from Internet)
    So, apart from removing the comments, I went with a solution that
    simply starts counting characters from the end of the string (from
    minus position), meaning, that if you go in positive direction - you
    move from the beginning to the end. if in negative - from end to
    beginning

--- Confirm the Ending.js
+++ Confirm the Ending.js
@@ -1,9 +1,5 @@
 function confirmEnding(str, target) {
-  // "Never give up and good luck will find you."
-  // -- Falcor
-  
-  return str.substr(str.length - target.length) === target;
-  //return str;
+  return (str.substr(-target.length) === target);
 }
 
 confirmEnding("Bastian", "n");
