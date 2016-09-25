62843c977ba850a99b33af928adba43622854960
krisrimar <rimarkris@gmail.com>
2016-09-25 18:16:06 +0300

    Own refactor commit (15 min)
    Removed the unnecessary “num” var and instead started working with
    newArr only

--- return largest number in array.js
+++ return largest number in array.js
@@ -1,16 +1,15 @@
 function largestOfFour(arr) {
     // You can do this!
-    var i = 0, k = 1, newArr = [], num;
-    for (i; i < arr.length; i++) {
-        num = arr[i][0];
-        for (k; k < arr[i].length; k++) {
-            arr[i][k] > num ? num = arr[i][k] : num = num;
+    var i, k, newArr = [];
+    for (i = 0; i < arr.length; i++) {
+        newArr[i] = arr[i][0];
+        for (k = 1; k < arr[i].length; k++) {
+            arr[i][k] > newArr[i] ? newArr[i] = arr[i][k] : newArr[i];
         }
         k = 0;
-        newArr.push(num);
-        num = 0;
     }
     console.log(newArr);
     return newArr;
 }
+
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
